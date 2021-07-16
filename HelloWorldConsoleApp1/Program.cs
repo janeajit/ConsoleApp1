14640a5cd6a7c5cd66e1c5d42aa6554c9e2fa925
Jane Pallikunnath <Jane.Pallikunnath@derivco.co.uk>
2021-07-16 08:40:38 +0100

    commit

--- HelloWorldConsoleApp1/Program.cs
+++ HelloWorldConsoleApp1/Program.cs
@@ -27,6 +27,8 @@ namespace HelloWorldConsoleApp1
 
         }
     }
+
+
     class Program
     {
 
@@ -38,6 +40,14 @@ namespace HelloWorldConsoleApp1
             Console.WriteLine("c1" + c.ajit);
            
             Console.WriteLine("c2" + c1.Example2);
+
+            "body": "{"Events":[{"IdempotentId":"30627997-a747-4c4b-a017-cdfe3a844676"," +
+                ""Type":"notifications"," +
+                ""Version":"1.0.0.0"," +
+                ""Date":"2021-07-13T22:53:19.621495Z"," +
+                ""Payload":{"SpinSportVisitId":"e6e7bae1-6f91-45b6-bdef-87449e655b8e"," +
+                ""NotificationType":"slotSessionStatus"," +
+                ""LossLimitPercentageRemaining\":10,\"TotalWagers\":45656.0,\"TotalPayouts\":1544.0,\"TotalNetWin\":233.0,\"GameSessionLimitValidation\":21,\"NotificationStartTime:"2021-07-13T11:53:18.000Z"}}],"CorrelationId":"df71519e-9fed-4d80-96ec-5a47fd751ad9"}"
         }
     }
 }
